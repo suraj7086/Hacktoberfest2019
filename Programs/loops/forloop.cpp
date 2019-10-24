1035f67028c1a744603815c91393fd9431f39ce5
Sanyog Jain <sanyogjain5678@gmail.com>
2019-10-25 01:19:54 +0530

    Created for loop

---
+++ Programs/loops/forloop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"C++ program to implement for loop"<<endl;
+  cout<<"Enter the limit till where numbers should be printed"<<endl;
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++)
+    cout<<i<<endl;
+   cout<<"End of for loop";
+   return 0;
+}
